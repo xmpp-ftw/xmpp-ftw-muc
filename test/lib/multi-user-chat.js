d89277c1920accbb4d41153fb8185f21113ff5a1
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-12 22:20:49 +0100

    Added test for joining a MUC room. Fixes #2

--- test/lib/multi-user-chat.js
+++ test/lib/multi-user-chat.js
@@ -13,7 +13,7 @@ describe('MultiUserChat', function() {
         socket.removeAllListeners('xmpp.muc.message')
     }
 
-    beforeEach(function() {
+    before(function() {
         socket = new helper.Eventer()
         xmpp = new helper.Eventer()
         manager = {
@@ -47,6 +47,7 @@ describe('MultiUserChat', function() {
 
         it('Shoudn\'t handle packets from other sources', function() {
             var room = 'fire@coven.witches.lit'
+            muc.rooms = []
             muc.handles(ltx.parse('<iq from="' + room + '" />')).should.be.false
         })
 
@@ -175,6 +176,54 @@ describe('MultiUserChat', function() {
             })
             var stanza = helper.getStanza('presence-join')
             muc.handle(stanza).should.be.true
+        })
+    })
+
+    describe('Can join a MUC room', function() {
+
+        it('Returns error if \'room\' key not provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'room' key")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.muc.join', {})
+        })
+
+        it('Returns error if \'nick\' key not provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'nick' key")
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            var request = { room: 'fire@coven@witches.lit' }
+            socket.emit('xmpp.muc.join', request)
+        })
+
+        it('Sends the expected stanza', function(done) {
+            var request = { room: 'fire@coven.witches.lit', nick: 'caldron' }
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('presence').should.be.true
+                stanza.attrs.to.should.equal(request.room + '/' + request.nick)
+                stanza.getChild('x', muc.NS).should.exist
+                muc.rooms.indexOf(request.room).should.be.above(-1)
+                done()
+            })
+            socket.emit('xmpp.muc.join', request, function(error, success) {
+                
+            })
         })
     })
 })
