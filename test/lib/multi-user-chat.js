a532665fc1fca4066e2ca0f8a34f9e306f95a33d
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-12 19:15:48 +0100

    Added initial basic tests

--- test/lib/multi-user-chat.js
+++ test/lib/multi-user-chat.js
@@ -7,7 +7,7 @@ describe('MultiUserChat', function() {
 
     var muc, socket, xmpp, manager
 
-    before(function() {
+    beforeEach(function() {
         socket = new helper.Eventer()
         xmpp = new helper.Eventer()
         manager = {
@@ -25,6 +25,16 @@ describe('MultiUserChat', function() {
     })
 
     describe('Can handle incoming requests', function() {
-    
+
+        it('Should handle packets from a joined room', function() {
+            var room = 'fire@coven.witches.lit'
+            muc.rooms.push(room)
+            muc.handles(ltx.parse('<iq from="' + room + '" />')).should.be.true
+        })
+
+        it('Shoudn\'t handle packets from other sources', function() {
+            var room = 'fire@coven.witches.lit'
+            muc.handles(ltx.parse('<iq from="' + room + '" />')).should.be.false
+        })
     })
 })
