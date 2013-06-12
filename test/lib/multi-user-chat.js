0c70c8eac211fb73bb61c3d276cca35b71e90647
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-12 19:53:04 +0100

    Add test for incoming message error

--- test/lib/multi-user-chat.js
+++ test/lib/multi-user-chat.js
@@ -32,9 +32,45 @@ describe('MultiUserChat', function() {
             muc.handles(ltx.parse('<iq from="' + room + '" />')).should.be.true
         })
 
+        it('Should ignore response on incoming messages', function() {
+            var room = 'fire@coven.witches.lit'
+            muc.rooms.push(room) 
+            muc.handles(ltx.parse('<message from="' + room + '/cauldron" />'))
+                .should.be.true
+        })
+
         it('Shoudn\'t handle packets from other sources', function() {
             var room = 'fire@coven.witches.lit'
             muc.handles(ltx.parse('<iq from="' + room + '" />')).should.be.false
+        })
+
+        describe('Incoming message stanzas', function() {
+
+            it('<message /> of type error', function(done) {
+                socket.once('xmpp.muc.message', function() {
+                    done('Unexpected event on \'xmpp.muc.message\'')
+                })
+                socket.once('xmpp.muc.room.config', function() {
+                    done('Unexpected event on \'xmpp.muc.room.config\'')
+                })
+                socket.once('xmpp.muc.error', function(error) {
+                    error.type.should.equal('message')
+                    error.error.should.eql(
+                        { type: 'modify', condition: 'bad-request' }
+                    )
+                    error.room.should.equal('fire@coven.witches.lit')
+                    error.content.should.equal('Are you of woman born?')
+                    socket.removeAllListeners()
+                    done()
+                })
+                var stanza = helper.getStanza('message-error')
+                muc.handle(stanza).should.be.true                
+            })
+
+        })
+
+        describe('Incoming presence stanzas', function() {
+
         })
     })
 })
