e3e82b4631474b8df153960aa8489693ac5b3f10
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-15 13:19:24 +0100

    Added tests for room configuration setting

--- test/lib/multi-user-chat.js
+++ test/lib/multi-user-chat.js
@@ -442,7 +442,115 @@ describe('MultiUserChat', function() {
                 )
             })
         })
-   
+
+        describe('Set configuration', function() {
+
+            it('Errors if \'room\' key missing', function(done) {
+                xmpp.once('stanza', function() {
+                    done('Unexpected outgoing stanza')
+                })
+                socket.once('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.description.should.equal("Missing 'room' key")
+                    error.request.should.eql(request)
+                    xmpp.removeAllListeners('stanza')
+                    done()
+                })
+                var request = {}
+                socket.emit('xmpp.muc.room.config.set', request)
+            })   
+
+            it('Errors if \'form\' key missing', function(done) {
+                xmpp.once('stanza', function() {
+                    done('Unexpected outgoing stanza')
+                })
+                socket.once('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.description.should.equal("Missing 'form' key")
+                    error.request.should.eql(request)
+                    xmpp.removeAllListeners('stanza')
+                    done()
+                })
+                var request = { room: 'fire@witches.coven.lit' }
+                socket.emit('xmpp.muc.room.config.set', request)
+            })
+
+            it('Handles invalid data form', function(done) {
+                xmpp.once('stanza', function() {
+                    done('Unexpected outgoing stanza')
+                })
+                socket.once('xmpp.error.client', function(error) {
+                    error.type.should.equal('modify')
+                    error.condition.should.equal('client-error')
+                    error.description.should.equal("Badly formatted data form")
+                    error.request.should.eql(request)
+                    xmpp.removeAllListeners('stanza')
+                    done()
+                })
+                var request = { room: 'fire@witches.coven.lit', form: true }
+                socket.emit('xmpp.muc.room.config.set', request)
+            })
+
+            it('Handles error response stanza', function(done) {
+                var room = 'fire@coven.witches.lit'
+                xmpp.once('stanza', function(stanza) {
+                     stanza.is('iq').should.be.true
+                     stanza.attrs.type.should.equal('set')
+                     stanza.attrs.to.should.equal(room)
+                     should.exist(stanza.attrs.id)
+                     stanza.getChild('query', muc.NS_OWNER).should.exist
+                     manager.makeCallback(helper.getStanza('iq-error'))
+                })
+                var callback = function(error, success) {
+                    should.not.exist(success)
+                    error.should.eql({
+                        type: 'cancel',
+                        condition: 'error-condition'
+                    })
+                    done()
+                }
+                socket.emit(
+                    'xmpp.muc.room.config.set',
+                    { room: room, form: [] },
+                    callback
+                )
+            })
+
+            it('Allows setting of room configuration', function(done) {
+                var request = {
+                    room: 'fire@coven.witches.lit',
+                    form: []
+                }
+                xmpp.once('stanza', function(stanza) {
+                     stanza.is('iq').should.be.true
+                     stanza.attrs.type.should.equal('set')
+                     stanza.attrs.to.should.equal(request.room)
+                     should.exist(stanza.attrs.id)
+                     stanza.getChild('query', muc.NS_OWNER).should.exist
+                     var x = stanza.getChild('query').getChild('x')
+                     x.attrs.xmlns.should.equal('jabber:x:data')
+                     x.attrs.type.should.equal('submit')
+                     x.children.length.should.equal(1)
+                     x.children[0].attrs.var.should.equal('FORM_TYPE')
+                     x.children[0].getChild('value').getText()
+                         .should.equal(muc.NS_CONFIG)
+                     manager.makeCallback(helper.getStanza('iq-result'))
+                })
+                var callback = function(error, data) {
+                    should.not.exist(error)
+                    data.should.be.true
+                    done()
+                }
+                socket.emit(
+                    'xmpp.muc.room.config.set',
+                    request,
+                    callback
+                )
+            })
+
+        })
     })
 
 })
