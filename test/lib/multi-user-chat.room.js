00569e564ec1f6dd38b10b784890b61435e6f281
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-09-28 14:52:28 +0100

    Code in place to create an instant room

--- test/lib/multi-user-chat.room.js
+++ test/lib/multi-user-chat.room.js
@@ -1,6 +1,7 @@
 var should        = require('should')
   , MultiUserChat = require('../../lib/multi-user-chat')
   , helper        = require('../helper')
+  , dataForm      = require('xmpp-ftw/lib/utils/xep-0004')
 
 describe('MUC Rooms', function() {
 
@@ -150,6 +151,112 @@ describe('MUC Rooms', function() {
                 { room: room },
                 callback
             )
+        })
+        
+    })
+    
+    describe.only('Create a room', function() {
+        
+        it('Errors when no callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.muc.create', {})
+        })
+
+        it('Errors when non-function callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing callback")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.muc.create', {}, true)
+        })
+        
+
+        it('Errors if \'room\' key missing', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal("Missing 'room' key")
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit('xmpp.muc.create', {}, callback)
+        })
+        
+        it('Errors if \'form\' provided and not valid', function(done) {
+            done('Not implemented yet')
+        })
+        
+        it('Sends expected stanza', function(done) {
+            var request = { room: 'fire@coven.witches.lit' }
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.id.should.exist
+                stanza.attrs.type.should.equal('set')
+                stanza.attrs.to.should.equal(request.room)
+                var x = stanza
+                    .getChild('query', muc.NS_OWNER)
+                    .getChild('x', dataForm.NS)
+                x.should.exist
+                x.attrs.type.should.equal('submit')
+                x.children.length.should.equal(0)
+                done()
+            })
+            socket.emit('xmpp.muc.create', request, function() {}) 
+        })
+        
+        it('Sends expected stanza with data form', function(done) {
+            done('Not implemented yet')
+        })
+        
+        it('Handles error response', function(done) {
+            var request = { room: 'fire@coven.witches.lit' }
+            xmpp.once('stanza', function() {
+                manager.makeCallback(helper.getStanza('iq-error'))
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.should.eql({
+                    type: 'cancel',
+                    condition: 'error-condition'
+                })
+                done()
+            }
+            socket.emit('xmpp.muc.create', request, callback)
+        })
+        
+        it('Handes success response', function(done) {
+            var request = { room: 'fire@coven.witches.lit' }
+            xmpp.once('stanza', function() {
+                manager.makeCallback(helper.getStanza('iq-result'))
+            })
+            var callback = function(error, success) {
+                should.not.exist(error)
+                success.should.be.true
+                done()
+            }
+            socket.emit('xmpp.muc.create', request, callback)
         })
         
     })
