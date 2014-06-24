fee00f1b5ffc9ff32127b1f112f1cfc83955ebe9
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-06-24 14:48:17 +0100

    Create rooms properly, fixes #48

--- test/lib/multi-user-chat.room.js
+++ test/lib/multi-user-chat.room.js
@@ -216,103 +216,47 @@ describe('MUC Rooms', function() {
             socket.send('xmpp.muc.create', {}, callback)
         })
 
-        it('Errors if \'form\' provided and not valid', function(done) {
-            var request = { room: 'fire@witches.coven.lit', form: true }
-            xmpp.once('stanza', function() {
-                done('Unexpected outgoing stanza')
-            })
-            var callback = function(error, success) {
-                should.not.exist(success)
-                error.type.should.equal('modify')
-                error.condition.should.equal('client-error')
-                error.description.should.equal('Badly formatted data form')
-                error.request.should.eql(request)
-                xmpp.removeAllListeners('stanza')
-                done()
-            }
-            socket.send('xmpp.muc.create', request, callback)
-        })
-
         it('Sends expected stanza', function(done) {
             var request = { room: 'fire@coven.witches.lit' }
             xmpp.once('stanza', function(stanza) {
-                stanza.is('iq').should.be.true
-                stanza.attrs.id.should.exist
-                stanza.attrs.type.should.equal('set')
+                stanza.is('presence').should.be.true
                 stanza.attrs.to.should.equal(request.room)
                 var x = stanza
-                    .getChild('query', muc.NS_OWNER)
-                    .getChild('x', dataForm.NS)
+                    .getChild('x', muc.NS)
                 x.should.exist
-                x.attrs.type.should.equal('submit')
                 x.children.length.should.equal(0)
                 done()
             })
             socket.send('xmpp.muc.create', request, function() {})
         })
 
-        it('Sends expected stanza with data form', function(done) {
+        it('Sends expected stanza for instant room', function(done) {
             var request = {
                 room: 'fire@coven.witches.lit',
-                form: [
-                    { var: 'muc#roomconfig_roomname', value: 'Campfire' },
-                    { var: 'muc#roomconfig_enablelogging', value: false }
-                ]
+                instant: true
             }
             xmpp.once('stanza', function(stanza) {
                 var x = stanza
-                    .getChild('query', muc.NS_OWNER)
                     .getChild('x', dataForm.NS)
 
                 x.should.exist
                 x.attrs.type.should.equal('submit')
-                x.children.length.should.equal(3)
-
-                x.children[0].getName().should.equal('field')
-                x.children[0].attrs.var.should.equal('FORM_TYPE')
-                x.children[0].getChildText('value')
-                   .should.equal('http://jabber.org/protocol/muc#roomconfig')
-
-                x.children[1].attrs.var.should.equal(request.form[0].var)
-                x.children[1].getChildText('value')
-                    .should.equal(request.form[0].value)
-
-                x.children[2].attrs.var.should.equal(request.form[1].var)
-                x.children[2].getChildText('value')
-                    .should.equal('false')
-
                 done()
             })
             socket.send('xmpp.muc.create', request, function() {})
         })
-
-        it('Handles error response', function(done) {
-            var request = { room: 'fire@coven.witches.lit' }
-            xmpp.once('stanza', function() {
-                manager.makeCallback(helper.getStanza('iq-error'))
-            })
-            var callback = function(error, success) {
-                should.not.exist(success)
-                error.should.eql({
-                    type: 'cancel',
-                    condition: 'error-condition'
-                })
+        
+        it('Handles error creating a room', function(done) {
+            
+            socket.once('xmpp.muc.error', function(error) {
+                error.type.should.equal('presence')
+                error.room.should.equal('coven@chat.shakespeare.lit')
+                error.error.type.should.equal('cancel')
+                error.error.by.should.equal('coven@chat.shakespeare.lit')
+                error.error.condition.should.equal('not-allowed')
                 done()
-            }
-            socket.send('xmpp.muc.create', request, callback)
-        })
-
-        it('Handes success response', function(done) {
-            var request = { room: 'fire@coven.witches.lit' }
-            xmpp.once('stanza', function() {
-                manager.makeCallback(helper.getStanza('iq-result'))
             })
-            var callback = function(error, success) {
-                should.not.exist(error)
-                success.should.be.true
-                done()
-            }
-            socket.send('xmpp.muc.create', request, callback)
+            muc.handle(helper.getStanza('presence-room-create-error'))
         })
 
     })
